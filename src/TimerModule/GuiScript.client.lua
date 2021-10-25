a76b2ae970602aa968a24b21fbd2f2500a045447
Aerodymier <83131649+Aerodymier@users.noreply.github.com>
2021-10-25 20:53:25 +0300

    Added examples for both modes

--- src/TimerModule/GuiScript.client.lua
+++ src/TimerModule/GuiScript.client.lua
@@ -1,12 +1,25 @@
 local Timer = require(script.Parent.TimerModule.TimerModule)
 
-local newTimer = Timer.new("2021 12 12 20 00 00") -- YYYY MM DD HH MM SS, example: 2021 12 12 20 00 00
+local newTimer = Timer.new(DateTime.fromUnixTimestamp(1635193626), true) -- YYYY MM DD HH MM SS, example: 2021 12 12 20 00 00
 
 local gui = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("ScreenGui"):WaitForChild("TextLabel")
 
-while wait(1) do
+--[[
+while task.wait(1) do
 	local dt = DateTime.now()
 	local formattedText: string | boolean = newTimer:calculateUniversalTime(dt:FormatUniversalTime("YYYY MM DD HH mm ss", "en-us"))
+	if typeof(formattedText) == "boolean" then
+		gui.Text = "Ended!"
+		break
+	else
+		gui.Text = formattedText
+	ends
+end
+]]
+
+while task.wait(1) do
+	local dt = DateTime.now()
+	local formattedText: string | boolean = newTimer:calculateUniversalTime(dt)
 	if typeof(formattedText) == "boolean" then
 		gui.Text = "Ended!"
 		break
